6907d4a979065ce2f09e55c45ea071987310a210
suraj-webxstudio <suraj@webxstudio.in>
2023-07-24 11:52:51 +0530

    improving gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@
 
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
